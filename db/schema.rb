5b48d3f1724aa2248ac02c70fbca14c19d5a2939
swdecker <swdecker@gmail.com>
2019-09-30 15:39:18 -0500

    added pokemon class and pokemons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "type"
+    t.integer "weight"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
