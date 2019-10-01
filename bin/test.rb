e88be60af78239865b77121d8113570a32130595
Wookeun Song <wookeun.s@gmail.com>
2019-10-01 17:38:00 -0500

    game class is almost done

--- bin/test.rb
+++ bin/test.rb
@@ -1,16 +1,3 @@
 require_relative 'board'
 require_relative 'game'
 
-
-board = Board.new
-
-# board.showBoard(3)
-
-# board.dropPiece(3, USER_PIECE)
-# board.dropPiece(3, FILLIN_PIECE)
-# board.dropPiece(3, USER_PIECE)
-# board.dropPiece(3, USER_PIECE)
-
-
-game = Game.new(1,board)
-game.playGame(10)
