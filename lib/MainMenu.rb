9764273fccbecadd778791667eadf523130374ad
swdecker <swdecker@gmail.com>
2019-10-03 15:43:59 -0500

    added ascii art

---
+++ lib/MainMenu.rb
@@ -0,0 +1,135 @@
+def splash
+puts"                                  .::.".colorize(:yellow)
+puts"                                 .;:**'            AMC".colorize(:yellow)
+puts"                                 `                  0".colorize(:yellow)
+puts"     .:XHHHHk.              db.   .;;.     dH  MX   0".colorize(:yellow)
+puts"   oMMMMMMMMMMM       ~MM  dMMP :MMMMMR   MMM  MR      ~MRMN".colorize(:yellow)
+puts"   QMMMMMb  'MMX       MMMMMMP !MX' :M~   MMM MMM  .oo. XMMM 'MMM".colorize(:yellow)
+puts'     `MMMM.  )M> :X!Hk. MMMM   XMM.o"  .  MMMMMMM X?XMMM MMM>!MMP'.colorize(:yellow)
+puts"      'MMMb.dM! XM M'?M MMMMMX.`MMMMMMMM~ MM MMM XM `'' MX MMXXMM".colorize(:yellow)
+puts'       ~MMMMM~ XMM. .XM XM`"MMMb.~*?**~ .MMX M t MMbooMM XMMMMMP'.colorize(:yellow)
+puts'        ?MMM>  YMMMMMM! MM   `?MMRb.    `"""   !L"MMMMM XM IMMM'.colorize(:yellow)
+puts'         MMMX   "MMMM"  MM       ~%:           !Mh.""" dMI IMMP'.colorize(:yellow)
+puts"         'MMM.                                             IMX".colorize(:yellow)
+puts"          ~M!M                                             IMP".colorize(:yellow)
+   
+end
+
+def clear_screen
+    print "\e[2J\e[f"
+end
+
+def pokeball
+
+    puts "        ▄███████████▄        ".colorize(:white)
+    puts "     ▄███".colorize(:white)+"▓▓▓▓▓▓▓▓▓▓▓".colorize(:red)+"███▄     ".colorize(:white)
+    puts "    ███".colorize(:white)+"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓".colorize(:red)+"███    ".colorize(:white)
+    puts "   ██".colorize(:white)+"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓".colorize(:red)+"██   ".colorize(:white)
+    puts "  ██".colorize(:white)+"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓".colorize(:red)+"██  ".colorize(:white)
+    puts " ██".colorize(:white)+"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓".colorize(:red)+"██ ".colorize(:white)
+    puts "██".colorize(:white)+"▓▓▓▓▓▓▓▓▓".colorize(:red)+"███████".colorize(:white)+"▓▓▓▓▓▓▓▓▓".colorize(:red)+"██".colorize(:white)
+    puts "██".colorize(:white)+"▓▓▓▓▓▓▓▓".colorize(:red)+"██░░░░░██".colorize(:white)+"▓▓▓▓▓▓▓▓".colorize(:red)+"██".colorize(:white)
+    puts "██".colorize(:white)+"▓▓▓▓▓▓▓".colorize(:red)+"██░░███░░██".colorize(:white)+"▓▓▓▓▓▓▓".colorize(:red)+"██".colorize(:white)
+    puts "███████████░░███░░███████████".colorize(:white)
+    puts "██░░░░░░░██░░███░░██░░░░░░░██".colorize(:white)
+    puts "██░░░░░░░░██░░░░░██░░░░░░░░██".colorize(:white)
+    puts "██░░░░░░░░░███████░░░░░░░░░██".colorize(:white)
+    puts " ██░░░░░░░░░░░░░░░░░░░░░░░██ ".colorize(:white)
+    puts "  ██░░░░░░░░░░░░░░░░░░░░░██  ".colorize(:white)
+    puts "   ██░░░░░░░░░░░░░░░░░░░██   ".colorize(:white)
+    puts "    ███░░░░░░░░░░░░░░░███    ".colorize(:white)
+    puts "     ▀███░░░░░░░░░░░███▀     ".colorize(:white)
+    puts "        ▀███████████▀        ".colorize(:white)
+end
+
+def arbok
+puts %Q[                   _,.----'"""'---..._].colorize(:purple)
+puts %Q[              _,-'"                   `-..].colorize(:purple)
+puts %Q[           _,'                            `-.].colorize(:purple)
+puts %Q[         ,'                                  `-.].colorize(:purple)
+puts %Q[       ,'                                _,..._ `.].colorize(:purple)
+puts %Q[      /                               ,."     `:- L].colorize(:purple)
+puts %Q[    ,'                             |.'         / ||].colorize(:purple)
+puts %Q[   /            _,.-._             L        .-' -,'].colorize(:purple)
+puts %Q[  /        _,.-"      `.            `     __   .'].colorize(:purple)
+puts %Q[ j      _,"           ||||           `. ,-  _.'].colorize(:purple)
+puts %Q[.     ,' `-..________.-' |            |' ,-'].colorize(:purple)
+puts %Q[|   .' `--,.___       _,'| /`.        ` '].colorize(:purple)
+puts %Q[|   |     `._  '"""""'   . `_Y.        Y_].colorize(:purple)
+puts %Q[`._          `-...__      `.`-'        | `-,...___].colorize(:purple)
+puts %Q[   ``-,.._          `""--.._`.         |  /     _,+`-._].colorize(:purple)
+puts %Q[    .'    '--._             `-+      _ |./    ,"       |].colorize(:purple)
+puts %Q[   ,  _,...._  `..             `-.:L_,v-'"`-./_____     l].colorize(:purple)
+puts %Q[  .,-"       `-.| `,                 j       l     "`   |].colorize(:purple)
+puts %Q[  j             |  l`l       _,......|       |       `  |].colorize(:purple)
+puts %Q[  |       _,.---^.vll_   _,-'        |       |        l '].colorize(:purple)
+puts %Q[  |     ,"       _7,, """            |       |        _V].colorize(:purple)
+puts %Q[  '    .        /  |'`l              |.._   ,'     _,'].colorize(:purple)
+puts %Q[   .  j       ,'    |  `._           |   `""-----"'].colorize(:purple)
+puts %Q[    \ |      j      '     `--..,,,..j].colorize(:purple)
+puts %Q[     Y       |       l             /].colorize(:purple)
+puts %Q[      `.     |        l           /].colorize(:purple)
+puts %Q[        `.   `         `.      _,'].colorize(:purple)
+puts %Q[          `._ `.         `--..'].colorize(:purple)
+puts %Q[             `---...,,,...-"' mh].colorize(:purple)
+puts ""
+end
+
+def meowth
+
+     print"                                        .\n" 
+     print"                         |              |\n" 
+     print"                         |             ,|\n" 
+     print"                ,_       |\\            F'   ,.-\"\"`.\n" 
+     print"               /  `-._   |`           // ,-\"_,..  |\n" 
+     print"              |   ___ `. | \\ ,\"\"\"`-. /.-' ,'    ' |_....._\n" 
+     print"              |  /   `-.`.  L......|j j_.'      ` |       `._\n" 
+     print"              | |      _,'| |______|' | '-._     ||  ,.-.    `.\n" 
+     print"               L|    ,'   | |      | j      `-.  || '    `.    \\\n" 
+     print"___            | \\_,'     | |`\"----| |         `.||       |\\    \\\n" 
+     print" \"\"=+...__     `,'   ,.-.   |....._|   _....     Y \\      j_),..+=--\n" 
+     print"     `\"-._\"._  .   ,' |  `   \\    /  ,' |   \\     \\ j,..-\"_..+-\"  L\n" 
+     print"          `-._-+. j   !   \\   `--'  .   !    \\  ,.-\" _..<._  |    |\n" 
+     print"              |-. |   |    L        |   !     |  .-/'      `.|-.,-|\n" 
+     print"              |__ '   '    |        '   |    /    /|   `, -. |   j\n" 
+     print"        _..--'\"__  `-.___,'          `.___,.'  __/_|_  /   / '   |\n" 
+     print"   _.-_..---\"\"_.-\\                            .,...__\"\"--./L/_   |\n" 
+     print" -'\"\"'     ,\"\"  ,-`-.    .___.,...___,    _,.+\"      \"\"\"\"`-+-==++-\n" 
+     print"          / /  `.   )\"-.._`v \\|    V/  /-'    \\._,._.'\"-. /    /\n" 
+     print"          ` `.  )---.     `\"\"\\\\__  / .'        /    \\    Y\"._.'\n" 
+     print"           `\"'`\"     `-.     /|._\"\"_/         |  ,..   _ |  |\n" 
+     print"                        `\"\"\"' |  \"'           `. `-'  (_|,-.'\n" 
+     print"                               \\               |`       .`-\n" 
+     print"                                `.           . j`._    /\n" 
+     print"                                 |`.._     _.'|    `\"\"/\n" 
+     print"                                 |    /\"\"'\"   |  .\". j\n" 
+     print"                                .`.__j         \\ `.' |\n" 
+     print"                                j    |          `._.'\n" 
+     print"                               /     |\n" 
+     print"                              /,  ,  \\\n" 
+     print"                              \\|  |   L\n" 
+     print"                               `..|_..' mh\n" 
+
+end
+
+def muk
+     print("                             _,..-------.._\n");
+     print("                         _,-'              `-._\n");
+     print("                       ,\"               .---.._`.\n");
+     print("                      /----.       _._,.._\"\"\":\"`\"`.\n");
+     print("                     /`--\"\"     _.'.'  / .`-.'-\"'\"`\\_\n");
+     print("                    /\"\"-._`----\" .'  .' '   _.`     \\\"-._\n");
+     print("                   .      `.___.'   / ,' . |   |  ._  . .``-._\n");
+     print("                 _,' _.            .,' ,\"| |   |   .`.|'.' ,--\".\n");
+     print("               ,'    ||`._..____..\"' ,'--'.|   |    `.|'  .'\"-._|\n");
+     print("              /___...'|   :._     _,'      `'-. |     |  _,.._  |.\n");
+     print("             |,--.._|j  ,'   `'\"\"'         ||  `|     '.'_..._` | )\n");
+     print("            ,'      /  /                   ||   |    /',\"     ` '/|\n");
+     print("         ,-'   .,-./  /      _,..----..___,':`_'    /.'        `.' ._\n");
+     print("        /  __.._ `-\\.'-----\"'  _,...__      '\"     /.\"\"-..-\"\"\"-. |_  `._\n");
+     print("   __..:--\"_.--.`.  `---....-`\"..__.\" `-...      _.\"'\"''`---..__`._`\"\"__\\\n");
+     print(".-'  `\"\"''\"       `..--\"\"-.   _.--.._   _.-\"\"-_,'              _.._`-._  '_\n");
+     print("|\"\"-.    __.-----._ '....`._-\" _,-\"'`-..._,..`__...\"\"\"-.__  _.'_.._`-..:'\"-)\n");
+     print(" `\"\"`..-\"_...----..___)     `\"\"              `....------._\"\",-'    `..-'`\"\"\n");
+     print("      `\"                                                  `\" mh\n");
+end
